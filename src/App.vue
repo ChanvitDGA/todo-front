4a65368aa7e0f74ddbce88b62a8995ccdb640570
ChanvitDGA <chanvit.thanawatthanaphaisan@dga.or.th>
2025-04-24 19:25:57 +0700

    test env

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ref, onMounted } from "vue";
 import Swal from "sweetalert2";
 
-const apiUrl = import.meta.env;
+const apiUrl = process.env;
 const inputValue = ref("");
 const todos = ref([]);
 
